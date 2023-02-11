d1eb74dbe6960d723c00201d4ab571d20bb6c44f
Swayam Prakash Barik <prakashswayam792@example.com>
2023-02-11 16:10:42 +0530

    fixed the bug of testcontroller

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,48 +1,55 @@
-import connection from "../config/databaseConn.js"; 
+import connection from "../config/databaseConn.js";
+ 
+const runInsert = (insertData, date, student_id, test_id) => {
+    return new Promise((resolve, reject) => {
+        let uploadMarkQuery = `insert into marks values (?,?,?,?,?,?)`;
+        connection.query(uploadMarkQuery, [student_id, test_id, insertData.subject_id, insertData.mark_obtained, insertData.total_marks, date], (err, data) => {
 
-export const uploadMarks = async(req, res) => {
-    try{
-        let student_id = req.params.student_id;
-        let test_id = req.params.test_id;
-        let bodyData = req.body.inputField; 
-        var dateObj = new Date();
-        let date = dateObj.toJSON(); 
-        date = date.slice(0,10);
-        let f = 0; 
-        for(let i = 0; i < bodyData.length; i++){           
-            let uploadMarkQuery = `insert into marks values (?,?,?,?,?,?)`;
-            connection.query(uploadMarkQuery, [student_id, test_id, bodyData[i].subject_id, bodyData[i].mark_obtained, bodyData[i].total_marks,date], (err, data) => {           
-                 
-                    if(err){   
-                        f = 1;          
-                        return res.status(500).send({
-                            success: 0,
-                            error: "Unable to insert because the subject id or test id doesn't exist!"
-                        });                        
-                    }else{
-                        if(i == bodyData.length - 1 && f === 0){
-                            return res.status(200).send({
-                                success: 1,
-                                message: "Data uploaded successfully !"
-                            });   
-                        }   
-                    }
-            })     
-        }    
-    }catch(err){ 
-        return res.status(500).json({
-            success: 0,
-            error : "Something went wrong!"
-        });
-    } 
+            if (err) {
+                return reject(err);
+            } else {
+                return resolve(data)
+            }
+        })
+    })
 }
 
-export const schoolAllTest = (req, res) =>{
-    try{
+export const uploadMarks = async (req, res) => {
+    try {
+        let student_id = req.params.student_id;
+        let test_id = req.params.test_id;
+        let bodyData = req.body.inputField;
+        var dateObj = new Date();
+        let date = dateObj.toJSON();
+        date = date.slice(0, 10); 
+        for (let i = 0; i < bodyData.length; i++) {           
+                try {
+                    await runInsert(bodyData[i], date, student_id, test_id);                   
+                } catch (err) {
+                    return res.status(500).send({
+                        success: 0,
+                        error: err.sqlMessage
+                });                
+            }
+        }
+        return res.status(200).json({
+            success: 1,
+            message: "Data uploaded successfully !"
+        })
+    } catch (err) {
+        return res.status(500).json({
+            success: 0,
+            error: "Something went wrong!"
+        });
+    }
+}
+
+export const schoolAllTest = (req, res) => {
+    try {
         let school_id = req.params.school_id;
         let schoolAllTestQuery = `select test_id from test where school_id=?`;
         connection.query(schoolAllTestQuery, school_id, (err, data) => {
-            if(err){
+            if (err) {
                 return res.status(500).json({
                     success: 0,
                     error: err.sqlMessage
@@ -53,7 +60,7 @@ export const schoolAllTest = (req, res) =>{
                 testDetails: data
             })
         })
-    }catch(err){
+    } catch (err) {
         return res.status(500).json({
             success: 0,
             error: "Something went wrong"
@@ -62,12 +69,12 @@ export const schoolAllTest = (req, res) =>{
 }
 
 export const addTest = (req, res) => {
-    try{
+    try {
         let school_id = req.params.school_id;
-        let {class_id, test_name, test_date} = req.body;
+        let { class_id, test_name, test_date } = req.body;
         let addTestQuery = `insert into test(class_id, test_name,test_date, school_id) values (?,?,?,?)`;
         connection.query(addTestQuery, [class_id, test_name, test_date, school_id], (err, data) => {
-            if(err){
+            if (err) {
                 return res.status(500).json({
                     success: 0,
                     error: "Check for the school_id exist or not !"
@@ -78,7 +85,7 @@ export const addTest = (req, res) => {
                 message: "Data inserted successfully !"
             })
         })
-    }catch(err){
+    } catch (err) {
         return res.status(500).json({
             success: 0,
             error: "Something went wrong"
@@ -87,21 +94,21 @@ export const addTest = (req, res) => {
 }
 
 export const addSubject = (req, res) => {
-    try{
+    try {
         let school_id = req.params.school_id;
-        let {subject_name, class_id} = req.body;
+        let { subject_name, class_id } = req.body;
         let addSubjectQuery = `insert into subject(subject_name) values (?)`;
         connection.query(addSubjectQuery, [subject_name], (err, data) => {
-            if(err){
+            if (err) {
                 return res.status(500).json({
                     success: 0,
-                    error:  err.sqlMessage
+                    error: err.sqlMessage
                 });
             }
             let subject_id = data.insertId;
             let addSubjectDetailsQuery = `insert into school_subject(school_id, class_id, subject_id) values (?,?,?)`;
             connection.query(addSubjectDetailsQuery, [school_id, class_id, subject_id], (err, data) => {
-                if(err){
+                if (err) {
                     return res.status(500).json({
                         success: 0,
                         error: "Check for the school_id or subject_id already exist or not !"
@@ -113,7 +120,7 @@ export const addSubject = (req, res) => {
                 })
             })
         })
-    }catch(err) {
+    } catch (err) {
         return res.status(500).json({
             success: 0,
             error: "Something went wrong"
@@ -122,31 +129,31 @@ export const addSubject = (req, res) => {
 }
 
 export const getTestSubject = (req, res) => {
-    try{
+    try {
         let student_id = req.params.student_id;
         let getClassIdQuery = `select class_id, school_id from student where student_id=?`;
         let getSubjectQuery = `select s.subject_id, s.subject_name from subject s, school_subject ss where s.subject_id = ss.subject_id and ss.class_id=? and ss.school_id=?`
         connection.query(getClassIdQuery, student_id, (err, data) => {
-            if(err){
+            if (err) {
                 return res.status(500).json({
                     success: 0,
-                    error:  err.sqlMessage
+                    error: err.sqlMessage
                 });
             }
-            if(data.length == 0){
+            if (data.length == 0) {
                 return res.status(500).json({
                     success: 0,
-                    message:  "No data found"
+                    message: "No data found"
                 });
             }
             let class_id = data[0].class_id;
             let school_id = data[0].school_id;
-            
+
             connection.query(getSubjectQuery, [class_id, school_id], (err, data) => {
-                if(err){
+                if (err) {
                     return res.status(500).json({
                         success: 0,
-                        error:  err.sqlMessage
+                        error: err.sqlMessage
                     });
                 }
                 return res.status(200).json({
@@ -155,10 +162,10 @@ export const getTestSubject = (req, res) => {
                 })
             })
         })
-    }catch(err){
+    } catch (err) {
         return res.status(500).json({
             success: 0,
             error: "Something went wrong"
-        })  
+        })
     }
 }
